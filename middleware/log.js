b8c97a9797f554a22a62b14dd7042c3661c1007d
楊可倫 <kelunyang@outlook.com>
2021-04-18 10:26:28 +0800

    增加對登入失敗的紀錄

--- middleware/log.js
+++ middleware/log.js
@@ -1,15 +1,18 @@
 const moment = require('moment');
 
+console.log('aaaq');
+
 module.exports = (data) => {
-    return async (req, res, next) => {
-        let authMapping = await require('./mapping')(data.models);
-        let action = authMapping[data.action];
-        await data.models.logModel.create({ 
-            tick: moment().unix(),
-            name: data.user ? req.session.passport.user : authMapping.nobodyAccount,
-            where: action.where,
-            action: action.action + "[" + data.comment + "]"
-        });
-        next();
-    }
+  return async (req, res, next) => {
+    let authMapping = await require('./mapping')(data.models);
+    let action = authMapping[data.action];
+    let comment = data.action === 'loginFail' ? '使用帳號密碼組合為' + req.app.locals.username + '/' + req.app.locals.password : data.comment;
+    await data.models.logModel.create({ 
+      tick: moment().unix(),
+      name: data.user ? req.session.passport.user : authMapping.nobodyAccount,
+      where: action.where,
+      action: action.action + "[" + comment + "]"
+    });
+    next();
+  }
 }
--- routes/backend.js
+++ routes/backend.js
@@ -9,8 +9,9 @@ const qs = require('qs');
 
 /* GET users listing. */
 module.exports = (app, passport, models) => {
-
   router.post('/login', auth(models), (req, res, next) => {
+    req.app.locals.username = req.body.user;
+    req.app.locals.password = req.body.pass;
     if(res.locals.status.type === 0) {
       passport.authenticate('local', {
         successRedirect: '/backend/loginSuccess',
@@ -23,13 +24,16 @@ module.exports = (app, passport, models) => {
     res.json({
       loginStatus: 2
     });
+    delete req.app.locals.username;
+    delete req.app.locals.password;
     return;
   });
 
   router.post('/logout', auth(models), log({
     models: models,
     user: true,
-    action: 'logout'
+    action: 'logout',
+    comment: '用戶登出'
   }), (req, res) => {
     req.logout();
     delete req.session.passport;
@@ -43,30 +47,37 @@ module.exports = (app, passport, models) => {
   router.get('/loginSuccess', auth(models), log({
     models: models,
     user: true,
-    action: 'loginSuccess'
+    action: 'loginSuccess',
+    comment: '用戶登入成功'
   }), (req, res) => {
     req.session.save();
     res.json({
       loginStatus: 1,
     });
+    delete req.app.locals.username;
+    delete req.app.locals.password;
     return;
   });
   
   router.get('/loginFail', auth(models), log({
     models: models,
     user: false,
-    action: 'loginFail'
+    action: 'loginFail',
+    comment: ''
   }), (req, res) => {
     res.json({
       loginStatus: 0
     });
+    delete req.app.locals.username;
+    delete req.app.locals.password;
     return;
   });
   
   router.get('/lineNotify', auth(models), log({
     models: models,
     user: false,
-    action: 'lineNotify'
+    action: 'lineNotify',
+    comment: '發送LINE訊息'
   }), async (req, res) => {
     let robotSetting = await models.robotModel.findOne({}).exec();
     let settingModel = await models.settingModel.findOne({}).exec();
